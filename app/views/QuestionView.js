b891bee5506e133861656943abdc037ad65fb645
Mirko Dietrich <mirko.dietrich@l4m1.de>
2013-08-13 15:26:52 +0200

    radiobutton/checkbox UI logic

--- app/views/QuestionView.js
+++ app/views/QuestionView.js
@@ -14,7 +14,27 @@ define([
 	var QuestionView = Backbone.Marionette.ItemView.extend({
 
 		tagName:  'tr',
-		template: '#template-question'
+		template: '#template-question',
+
+		events: {
+			'click .radio-button': 'clickRadioButton',
+			'click .checkbox':     'clickCheckBox',
+		},
+
+		clickRadioButton: function(evt) {
+			var $t = this.$(evt.target);
+			if ($t.hasClass('selected')) {
+				$t.removeClass('selected');
+			}
+			else {
+				this.$('.radio-button').removeClass('selected');
+				$t.addClass('selected');
+			}
+		},
+
+		clickCheckBox: function(evt) {
+			this.$(evt.target).toggleClass('selected');
+		},
 
 	});
 
